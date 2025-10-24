068311ab558e949c9c7d1456a5f76c1ef9359a6d
Sky Yin <sky.yin@gmail.com>
2025-10-24 14:58:50 -0700

    Add assets to visualize Manhattan taxi trip data
    - Implemented `manhattan_map` asset to generate a map visualizing the number of trips per taxi zone in Manhattan.
    - Added `trips_by_week` asset to aggregate and save weekly taxi trip statistics to a CSV file.

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/metrics.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/metrics.py
@@ -32,3 +32,44 @@ def manhattan_stats() -> None:
 
     with open(constants.MANHATTAN_STATS_FILE_PATH, "w") as output_file:
         output_file.write(trips_by_zone.to_json())
+
+
+# src/dagster_essentials/defs/assets/metrics.py
+@dg.asset(
+    deps=["manhattan_stats"],
+)
+def manhattan_map() -> None:
+    trips_by_zone = gpd.read_file(constants.MANHATTAN_STATS_FILE_PATH)
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+    trips_by_zone.plot(
+        column="num_trips", cmap="plasma", legend=True, ax=ax, edgecolor="black"
+    )
+    ax.set_title("Number of Trips per Taxi Zone in Manhattan")
+
+    ax.set_xlim([-74.05, -73.90])  # Adjust longitude range
+    ax.set_ylim([40.70, 40.82])  # Adjust latitude range
+
+    # Save the image
+    plt.savefig(constants.MANHATTAN_MAP_FILE_PATH, format="png", bbox_inches="tight")
+    plt.close(fig)
+
+
+@dg.asset(deps=["taxi_trips"])
+def trips_by_week() -> None:
+    query = """
+        select
+            date_trunc('week', pickup_datetime) as period,
+            count(1) as num_trips,
+            sum(passenger_count) as passenger_count,
+            sum(total_amount) as total_amount,
+            sum(trip_distance) as trip_distance
+        from trips
+        group by period
+    """
+
+    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+    trips_by_vendor = conn.execute(query).fetch_df()
+
+    with open(constants.TRIPS_BY_WEEK_FILE_PATH, "w") as output_file:
+        output_file.write(trips_by_vendor.to_csv(index=False))
